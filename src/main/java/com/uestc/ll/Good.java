8097d084da997ddebe7c45dc627a2fc4ff5c143c
liusen <liusen@example.com>
2019-07-11 19:47:00 +0800

    commit nononon

--- src/main/java/com/uestc/ll/Good.java
+++ src/main/java/com/uestc/ll/Good.java
@@ -4,5 +4,6 @@ public class Good {
 	public static void main(String[] args) {
 		System.out.println("Hello World\nHello Git");
 		System.out.println("okkkkk");
+		System.out.println("nonononon");
 	}
 }
